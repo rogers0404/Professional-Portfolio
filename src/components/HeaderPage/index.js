0f2bbcaee97eadfddb2c0cc467c01f5d006ad4fd
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-08 13:45:28 -0500

    Added File structure and Initialized app

---
+++ src/components/HeaderPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import coverImageHeader from '../../assets/header-background.png';
+function HeaderPage() {
+  return (
+    <header className="headerImage">
+        <div className="my-5">
+            <h1 id="header" >Rogers</h1>
+
+        </div>
+      
+    </header>
+  );
+}
+
+export default HeaderPage;
