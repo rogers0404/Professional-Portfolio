9f1ba63ddbf0139b5b74f7386a87208fb27108ae
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-09 00:43:58 -0500

    Added footer to the profile

--- src/components/FooterPage/index.js
+++ src/components/FooterPage/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
-//import About from '../About';
-function FooterPage(props) {
+import twitterImage from "../../assets/twitter.png";
+import githubImage from "../../assets/GitHub.png";
+import linkedinImage from "../../assets/linkedin.png";
+import emailImage from "../../assets/email.png";
+/* import twitter from "../../assets/twitter.png"; */
+function FooterPage() {
 
   return (
-    <footer className="footerImage" />
+    <footer className="footerImage">
+        <div class="p-1 m-0 mw-100 d-flex justify-content-center">
+          <div>
+             <a className="text-white" href="https://github.com/rogers0404/" target="blank">
+                <img className="img-fluid" src={githubImage} alt="Rogers"  style={{width: "8%"}}/>
+            </a>
+          </div>
+          <div>
+             <a className="text-white" href="https://twitter.com/rogersalberto" target="blank">
+             <img className="img-fluid" src={twitterImage} alt="Rogers" style={{width: "8%"}}/>
+            </a>
+          </div>
+          <div>
+             <a className="text-white" href="https://www.linkedin.com/in/rogers-ramirez-496a66b6" target="blank">
+             <img className="img-fluid" src={linkedinImage} alt="Rogers" style={{width: "8%"}}/>
+            </a>
+          </div>
+          <div>
+             <a className="text-white" href="mailto:rogers.ramirez2008@gmail.com" target="blank">
+             <img className="img-fluid" src={emailImage} alt="Rogers" style={{width: "8%"}}/>
+            </a>
+          </div>
+        </div>
+    </footer>
   );
 }
 
