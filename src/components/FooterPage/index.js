bca519cfcd7ee50fa0ae0e9acf28e2b683871324
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-09 19:28:01 -0500

    Fixed styling in Footer Resume section

--- src/components/FooterPage/index.js
+++ src/components/FooterPage/index.js
@@ -7,26 +7,26 @@ import emailImage from "../../assets/email.png";
 function FooterPage() {
 
   return (
-    <footer className="footerImage">
-        <div className="p-1 m-0 mw-100 d-flex justify-content-center">
+    <footer className="footerImage d-flex justify-content-center">
+        <div className="p-1 m-3 d-flex justify-content-center w-25 align-items-center">
           <div>
              <a className="text-white" href="https://github.com/rogers0404/" target="blank">
-                <img className="img-fluid" src={githubImage} alt="Rogers"  style={{width: "8%"}}/>
+                <img className="img-fluid" src={githubImage} alt="Rogers" style={{width: "40%"}}/>
             </a>
           </div>
           <div>
              <a className="text-white" href="https://twitter.com/rogersalberto" target="blank">
-             <img className="img-fluid" src={twitterImage} alt="Rogers" style={{width: "8%"}}/>
+             <img className="img-fluid" src={twitterImage} alt="Rogers" style={{width: "40%"}}/>
             </a>
           </div>
           <div>
              <a className="text-white" href="https://www.linkedin.com/in/rogers-ramirez-496a66b6" target="blank">
-             <img className="img-fluid" src={linkedinImage} alt="Rogers" style={{width: "8%"}}/>
+             <img className="img-fluid" src={linkedinImage} alt="Rogers" style={{width: "40%"}}/>
             </a>
           </div>
           <div>
              <a className="text-white" href="mailto:rogers.ramirez2008@gmail.com" target="blank">
-             <img className="img-fluid" src={emailImage} alt="Rogers" style={{width: "8%"}}/>
+             <img className="img-fluid" src={emailImage} alt="Rogers" style={{width: "40%"}}/>
             </a>
           </div>
         </div>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,35 +8,43 @@ function Resume() {
                 <div className="card-body justify-content-left">
                 <a 
                   className="list-group-item list-group-item-action active text-white " 
-                  href="https://drive.google.com/file/d/1NdfqOehhR4KH7YYGpMk7PSqL87CpKXwz/view?usp=sharing">
+                  href="https://drive.google.com/file/d/1NdfqOehhR4KH7YYGpMk7PSqL87CpKXwz/view?usp=sharing"
+                  target="blank">
                     Download my Resume
                 </a>
-                    <p className="card-text text-dark m-1 text-justify">
-                          Front-End Proficiencies
-                      </p>
-                        <ul className="list-group">
-                            <li className="list-group-item disabled">HTML</li>
-                            <li className="list-group-item">CSS</li>
-                            <li className="list-group-item">JavaScript</li>
-                            <li className="list-group-item">JQuery</li>
-                            <li className="list-group-item">Responsive Design</li>
-                            <li className="list-group-item">React</li>
-                            <li className="list-group-item">Bootstrap</li>
-                            <li className="list-group-item">CURB Foundation</li>
-                        </ul>
-                        <p className="card-text text-dark m-1 text-justify">
-                          Back-End Proficiencies
-                      </p>
-                      <ul className="list-group">
-                            <li className="list-group-item">APIs</li>
-                            <li className="list-group-item">Node.js</li>
-                            <li className="list-group-item">Express.js</li>
-                            <li className="list-group-item">MySQL, Sequelize</li>
-                            <li className="list-group-item">MongoDB, Mogoose</li>
-                        </ul>
-                </div>
-                <div className="card-footer text-muted">
-                    2 days ago
+                <div className="card-deck">                        
+                    <div className="card">
+                      <div className="card-body justify-content-left">
+                          <p className="card-text text-dark m-1 text-justify">
+                              Front-End Proficiencies
+                          </p>
+                            <ul className="list-group">
+                                <li className="list-group-item">HTML</li>
+                                <li className="list-group-item">CSS</li>
+                                <li className="list-group-item">JavaScript</li>
+                                <li className="list-group-item">JQuery</li>
+                                <li className="list-group-item">Responsive Design</li>
+                                <li className="list-group-item">React</li>
+                                <li className="list-group-item">Bootstrap</li>
+                                <li className="list-group-item">CURB Foundation</li>
+                            </ul>
+                      </div>
+                    </div>
+                    <div className="card">
+                      <div className="card-body justify-content-left">
+                            <p className="card-text text-dark m-1 text-justify">
+                              Back-End Proficiencies
+                          </p>
+                          <ul className="list-group">
+                                <li className="list-group-item">APIs</li>
+                                <li className="list-group-item">Node.js</li>
+                                <li className="list-group-item">Express.js</li>
+                                <li className="list-group-item">MySQL, Sequelize</li>
+                                <li className="list-group-item">MongoDB, Mogoose</li>
+                            </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
