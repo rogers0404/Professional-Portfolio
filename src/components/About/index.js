e1a1b3070aff5019a7934a0f5d035006014f2f53
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-09 20:47:43 -0500

    Styling About Section and fixing bugs

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,19 +9,25 @@ function About() {
                 <div className="card-header text-dark m-1">About Me</div>
                 <div className="card-body">
                     <img className="card-img-top m-1" src={photo} alt="Rogers" style={{width: "10%"}}/>
-                    <div className="d-flex justify-content-left">
-                      <p className="card-text text-dark m-1 text-justify">
-                          Full Stack Web Developer and Instrumentation Engineer with experience in Oil and Gas Industry. 
-                          Certificate in full stack web developer from University of Central Florida. 
-                          Skills in web technologies such as JavaScript, CSS, Git, Agile development and strengths in working under pressure, 
-                          well-organized, collaborative, teamwork and detail-oriented. Excited to develop Web Sites bring me to face the problem 
-                          with passion and dealing with the deadline. All those aptitudes guided me to accomplish every project I worked on. 
-                          For these reasons, it would be helpful for any web developer team
-                      </p>
+                    <div>
+                        <p className="card-text text-dark m-1 text-justify">
+                        I was born in Venezuela, near Orinoco River in a City called Ciudad Bolivar, Bolivar State in South Venezuela. 
+                        I love coding, watching movies and play football soccer.
+                          </p>
+                      </div>
+                      <div>
+                        <p><br></br></p>
+                      </div>
+                      <div>
+                          <p className="card-text text-dark m-1 text-justify">
+                              Full Stack Web Developer and Instrumentation Engineer with experience in Oil and Gas Industry. 
+                              Certificate in full stack web developer from University of Central Florida. 
+                              Skills in web technologies such as JavaScript, CSS, Git, Node.js Express, Agile development and strengths in working under pressure, 
+                              well-organized, collaborative, teamwork and detail-oriented. Excited to develop Web Sites and face the problem 
+                              with passion and dealing with the deadline. All those aptitudes needed to accomplish every project. 
+                              For these reasons, it would be helpful for any web developer team
+                          </p>
                     </div>
-                </div>
-                <div className="card-footer text-muted">
-                    2 days ago
                 </div>
             </div>
         </div>
